f480d7b28d70cdea7c8e2d3fb4794824fe370e08
VasylPopaz <pafos1997@gmail.com>
2024-04-17 13:07:53 +0200

    added favicon and svg sprite

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,11 @@
+import Icons from "./sprite.svg";
+
+// Example: <Icon id="logo" className="icon" size={20} />
+
+export const Icon = ({ id, className, size }) => {
+  return (
+    <svg className={className} height={size} width={size}>
+      <use href={Icons + "#icon-" + id}></use>
+    </svg>
+  );
+};
