94358262423d59dfb7113dcc141f2d52304df245
skabanets <79872707+skabanets@users.noreply.github.com>
2024-04-18 20:47:16 +0000

    Deploying to gh-pages from @ DimkaGrek/NodeAvengers@f24e926ce8273d259bf1a1ce06647e2e0cd3c490 🚀

--- assets/Button-DSDWW4eh.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-BDmdZ3ia.js";const x=({type:t,onClick:o,children:r,className:s})=>n.jsx("button",{type:t,className:s,onClick:o,children:r});export{x as B};
---
+++ assets/HomePage-Cu8iMt3r.js
@@ -0,0 +1,1 @@
+import{j as e,L as o}from"./index--tgxQg-d.js";const s=()=>e.jsxs("div",{children:["HomePage",e.jsx(o,{to:"Project",children:"Board"})]});export{s as default};
