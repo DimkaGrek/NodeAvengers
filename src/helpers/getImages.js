ea134e4b8716cd5af19333ee905ac0978274c960
VasylPopaz <pafos1997@gmail.com>
2024-04-19 00:03:38 +0200

    fixed AddButton

--- src/helpers/getImages.js
+++ src/helpers/getImages.js
@@ -1,21 +1,21 @@
-import bgNone from "../assets/images/board-bg/prev-bg/bg-none.png";
-import bg1 from "../assets/images/board-bg/prev-bg/bg1.png";
-import bg2 from "../assets/images/board-bg/prev-bg/bg2.png";
-import bg3 from "../assets/images/board-bg/prev-bg/bg3.png";
-import bg4 from "../assets/images/board-bg/prev-bg/bg4.png";
-import bg5 from "../assets/images/board-bg/prev-bg/bg5.png";
-import bg6 from "../assets/images/board-bg/prev-bg/bg6.png";
-import bg7 from "../assets/images/board-bg/prev-bg/bg7.png";
-import bg8 from "../assets/images/board-bg/prev-bg/bg8.png";
-import bg9 from "../assets/images/board-bg/prev-bg/bg9.png";
-import bg10 from "../assets/images/board-bg/prev-bg/bg10.png";
-import bg11 from "../assets/images/board-bg/prev-bg/bg11.png";
-import bg12 from "../assets/images/board-bg/prev-bg/bg12.png";
-import bg13 from "../assets/images/board-bg/prev-bg/bg13.png";
-import bg14 from "../assets/images/board-bg/prev-bg/bg14.png";
-import bg15 from "../assets/images/board-bg/prev-bg/bg15.png";
+import bgNone from '../assets/images/board-bg/prev-bg/bg-none.png';
+import bg1 from '../assets/images/board-bg/prev-bg/bg1.png';
+import bg2 from '../assets/images/board-bg/prev-bg/bg2.png';
+import bg3 from '../assets/images/board-bg/prev-bg/bg3.png';
+import bg4 from '../assets/images/board-bg/prev-bg/bg4.png';
+import bg5 from '../assets/images/board-bg/prev-bg/bg5.png';
+import bg6 from '../assets/images/board-bg/prev-bg/bg6.png';
+import bg7 from '../assets/images/board-bg/prev-bg/bg7.png';
+import bg8 from '../assets/images/board-bg/prev-bg/bg8.png';
+import bg9 from '../assets/images/board-bg/prev-bg/bg9.png';
+import bg10 from '../assets/images/board-bg/prev-bg/bg10.png';
+import bg11 from '../assets/images/board-bg/prev-bg/bg11.png';
+import bg12 from '../assets/images/board-bg/prev-bg/bg12.png';
+import bg13 from '../assets/images/board-bg/prev-bg/bg13.png';
+import bg14 from '../assets/images/board-bg/prev-bg/bg14.png';
+import bg15 from '../assets/images/board-bg/prev-bg/bg15.png';
 
-export function getImages() {
+export const getImages = () => {
   return {
     images: [
       bgNone,
@@ -36,14 +36,14 @@ export function getImages() {
       bg15,
     ],
     icons: [
-      "project",
-      "star",
-      "loading",
-      "puzzle-piece",
-      "container",
-      "lightning",
-      "colors",
-      "hexagon",
+      'project',
+      'star',
+      'loading',
+      'puzzle-piece',
+      'container',
+      'lightning',
+      'colors',
+      'hexagon',
     ],
   };
-}
+};
