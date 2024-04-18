a2d9c71e4abca9902be6a572b6be86f6e6e2ec12
Sergey Kabanets <skabanets97@gmail.com>
2024-04-18 21:38:23 +0300

    add import modern-normalize

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import "./index.css";
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import './index.css';
+import 'modern-normalize/modern-normalize.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/NodeAvengers">
     <App />
   </BrowserRouter>
