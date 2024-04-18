f870f32c71688f4cb2c5ae1561b313213e2fef8c
VasylPopaz <pafos1997@gmail.com>
2024-04-18 22:24:40 +0200

    added Schema for EditBoard

--- src/components/EditBoardForm/EditBoardForm.jsx
+++ src/components/EditBoardForm/EditBoardForm.jsx
@@ -1,75 +1,88 @@
-import * as Yup from "yup";
+import { Formik, Form, Field } from 'formik';
 
-import Button from "../Button/Button";
-import { Icon } from "../Icon/Icon";
-import { AddButton } from "../AddButton/AddButton";
+import Button from '../Button/Button';
+import { Icon } from '../Icon/Icon';
+import { AddButton } from '../AddButton/AddButton';
 
-import { getImages } from "../../helpers/getImages";
+import { getImages } from '../../helpers/getImages';
+import { Schema } from '../../schemas/EditBoardSchema';
 
-import s from "./EditBoardForm.module.css";
-
-const Schema = Yup.object().shape({
-  title: Yup.string()
-    .min(3, "Your title should be 8 or more characters.")
-    .max(64, "Your title should not be more than 64 characters.")
-    .required("This field is required."),
-});
+import s from './EditBoardForm.module.css';
 
 export const EditBoardForm = ({ isEdit = false }) => {
   const { images, icons } = getImages();
 
   return (
-    <form className={s.form}>
-      <input className={s.input} type="text" placeholder="Title" />
-      <div className={s.wrapper}>
-        <p className={s.description}>Icons</p>
-        <ul className={s.iconList}>
-          {icons.map((icon, index) => (
-            <li key={index}>
-              <label>
-                <input
-                  className={s.radioBtn}
-                  type="radio"
-                  name="icon"
-                  value={index}
-                  defaultChecked={index === 0}
-                />
-                <Icon id={icon} className={s.icon} size={18} />
-              </label>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className={s.wrapper}>
-        {" "}
-        <p className={s.description}>Background</p>
-        <ul className={s.imageList}>
-          {images.map((image, index) => (
-            <li key={index}>
-              <label>
-                <input
-                  className={s.radioBtn}
-                  type="radio"
-                  name="image"
-                  value={index}
-                  defaultChecked={index === 0}
-                />
-                <img
-                  className={s.img}
-                  src={image}
-                  alt="background"
-                  width={28}
-                  height={28}
-                />
-              </label>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <Button type="submit" className={s.button}>
-        <AddButton width={28} height={28} iconSize={18} />
-        Create
-      </Button>
-    </form>
+    <Formik
+      initialValues={{
+        title: '',
+        icon: 0,
+        backgroundImage: 0,
+      }}
+      validationSchema={Schema}
+      onSubmit={values => {
+        console.log(values);
+      }}
+    >
+      {({ errors, touched, setFieldValue }) => (
+        <Form className={s.form}>
+          <div className={s.wrapper}>
+            <Field type="text" name="title" placeholder="Title" />
+            {errors.title && touched.title ? (
+              <p className={s.descrError}>{errors.title}</p>
+            ) : null}
+          </div>
+          <div className={s.wrapper}>
+            <p className={s.description}>Icons</p>
+            <ul className={s.iconList}>
+              {icons.map((icon, index) => (
+                <li key={index}>
+                  <label>
+                    <Field
+                      className={s.radioBtn}
+                      type="radio"
+                      name="icon"
+                      value={index}
+                      onChange={() => setFieldValue('icon', index)}
+                    />
+                    <Icon id={icon} className={s.icon} size={18} />
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={s.lastWrapper}>
+            {' '}
+            <p className={s.description}>Background</p>
+            <ul className={s.imageList}>
+              {images.map((image, index) => (
+                <li key={index}>
+                  <label>
+                    <Field
+                      className={s.radioBtn}
+                      type="radio"
+                      name="backgroundImage"
+                      value={index}
+                      onChange={() => setFieldValue('backgroundImage', index)}
+                    />
+                    <img
+                      className={s.img}
+                      src={image}
+                      alt="background"
+                      width={28}
+                      height={28}
+                    />
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <Button type="submit" className={s.button}>
+            {!isEdit && <AddButton width={28} height={28} iconSize={18} />}
+            Create
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 };
