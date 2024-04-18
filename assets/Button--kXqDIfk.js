f339302246d081a17448fde648f78864bcec6d60
VasylPopaz <145382665+VasylPopaz@users.noreply.github.com>
2024-04-18 19:21:52 +0000

    Deploying to gh-pages from @ DimkaGrek/NodeAvengers@437736f2906beb07871b42f5073f9daf88ccc005 🚀

--- assets/Button--kXqDIfk.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-BgRr3Wx1.js";const x=({type:t,onClick:o,children:r,className:s})=>n.jsx("button",{type:t,className:s,onClick:o,children:r});export{x as B};
---
+++ assets/Button-DSDWW4eh.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-BDmdZ3ia.js";const x=({type:t,onClick:o,children:r,className:s})=>n.jsx("button",{type:t,className:s,onClick:o,children:r});export{x as B};
