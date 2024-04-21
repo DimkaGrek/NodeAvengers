ac877be44987da3b7c7c7b2503c6dc4e7d7e4a20
EKhobotin <evgen.29.3.85@gmail.com>
2024-04-21 10:28:13 +0300

    sidebar (without svg)

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,49 +3,72 @@ import { useTheme } from '../../hooks/useTheme';
 import s from './Sidebar.module.css';
 import { Icon } from '../Icon/Icon';
 import flower from '../../assets/images/flower.png';
+import { AddButton } from '../AddButton/AddButton';
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutThunk } from '../../redux/auth/operations';
+import { useNavigate } from 'react-router-dom';
+import { selectRefreshToken } from '../../redux/auth/slice';
+import { toast } from 'react-toastify';
 
 const Sidebar = () => {
   const { setTheme } = useTheme();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const refreshToken = useSelector(selectRefreshToken);
+  const handleLogOut = async () => {
+    try {
+      await dispatch(logoutThunk({ refreshToken }));
+      navigate('/welcome');
+    } catch (error) {
+      toast.error('Something went wront please try again.');
+    }
+  };
   return (
-    <aside className={s.SidebarBlock}>
-      <h1 className={s.TitleHidden}>Task boards page</h1>
-      <NavLink className={s.NavLogo} to="/">
-        <Icon id="logo" className={s.SvgLightning} size={18} />
-        <p className={s.Title}>Task Pro</p>
+    <aside className={s.sideBarBlock}>
+      <h1 className={s.titleHidden}>Task boards page</h1>
+      <NavLink className={s.navLogo} to="/">
+        <Icon
+          id="logo"
+          className={s.svgLightning}
+          width={32}
+          height={32}
+          size={32}
+        />
+        <p className={s.title}>Task Pro</p>
       </NavLink>
-      <div className={s.Section}>
-        <h2 className={s.MyBoardTitle}>My boards</h2>
+      <div className={s.section}>
+        <h2 className={s.myBoardTitle}>My boards</h2>
 
-        <div className={s.BoardTitleBlock}>
-          <p>Create a new board</p>
-          <button className={s.ButtonAdd} aria-label="add" type="button">
-            <Icon id="plus" className={s.SvgPlus} size={18} />
+        <div className={s.boardTitleBlock}>
+          <p className={s.boardTitle}>Create a new board</p>
+          <button className={s.buttonAdd} aria-label="add" type="button">
+            <Icon id="plus" className={s.svgPlus} size={20} />
           </button>
         </div>
 
-        <div className={s.MainBoard}>
+        <div className={s.mainBoard}>
           <ul>
-            <li className={s.BoardList}>
-              <NavLink className={s.Nav} to="">
-                <div className={s.TitleBoard}>
-                  <Icon id="close" className={s.SvgLightning} size={18} />
-                  <p className={s.BoardTitle}>Project office</p>
+            <li className={s.boardList}>
+              <NavLink className={s.nav} to="">
+                <div className={s.titleBoard}>
+                  <Icon id="project" className={s.svgProject} size={18} />
+                  <p className={s.boardTitle}>Project office</p>
                 </div>
 
-                <div className={s.BoardIcons}>
+                <div className={s.boardIcons}>
                   <button
-                    className={s.ButtonIcon}
+                    className={s.buttonIcon}
                     type="button"
                     aria-label="edit"
                   >
-                    <Icon id="close" className={s.EditIcon} size={18} />
+                    <Icon id="pencil" className={s.editIcon} size={16} />
                   </button>
                   <button
-                    className={s.ButtonIcon}
+                    className={s.buttonIcon}
                     type="button"
                     aria-label="delete"
                   >
-                    <Icon id="close" className={s.EditIcon} size={18} />
+                    <Icon id="trash" className={s.editIcon} size={16} />
                   </button>
                 </div>
               </NavLink>
@@ -53,26 +76,31 @@ const Sidebar = () => {
           </ul>
         </div>
       </div>
-      <div className={s.NeedHelpBlock}>
+      <div className={s.needHelpBlock}>
         <img
-          className={s.Flower}
+          className={s.flower}
           src={flower}
           alt="flower"
           width={54}
           height={78}
         />
-        <p className={s.HelpContent}>
+        <p className={s.helpContent}>
           If you need help with <span>TaskPro</span>, check out our support
           resources or reach out to our customer support team.
         </p>
-        <button className={s.NeedHelpButton} type="button" name="help">
-          <Icon id="close" className={s.SvgHelp} size={18} />
+        <button className={s.needHelpButton} type="button" name="help">
+          <Icon id="help-circle" className={s.svgHelp} size={20} />
           Need help?
         </button>
       </div>
 
-      <button type="button" name="logout">
-        <Icon id="close" className={s.SvgLogout} size={18} />
+      <button
+        className={s.logoutBtn}
+        type="button"
+        name="logout"
+        onClick={handleLogOut}
+      >
+        <Icon id="login" className={s.svgLogout} size={18} />
         Log out
       </button>
     </aside>
