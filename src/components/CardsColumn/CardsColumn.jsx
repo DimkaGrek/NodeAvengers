e316ce4fd08b4d85e0543f538b952c82c08c516d
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-04-21 13:00:22 +0300

    add add card base logic

--- src/components/CardsColumn/CardsColumn.jsx
+++ src/components/CardsColumn/CardsColumn.jsx
@@ -2,9 +2,16 @@ import s from './CardsColumn.module.css';
 import { Icon } from '../../components/Icon/Icon.jsx';
 import Card from '../../components/Card/Card.jsx';
 import { AddButton } from '../../components/AddButton/AddButton.jsx';
+import { useState } from 'react';
 
 const CardsColumn = () => {
+  const [addCard, setAddCard] = useState([]);
+
   const columnTitle = 'To Do';
+
+  const handleAddCardButton = () => {
+    setAddCard([...addCard, <Card key={addCard.length} />]);
+  };
 
   return (
     <div className={s.singleColumnWrapper}>
@@ -21,13 +28,12 @@ const CardsColumn = () => {
       </div>
 
       <div className={s.cardsContainer}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {addCard.map((card, index) => (
+          <div key={index}>{card}</div>
+        ))}
       </div>
 
-      <button className={s.addCardBtn}>
+      <button className={s.addCardBtn} onClick={handleAddCardButton}>
         <AddButton color="dark" width={28} height={28} iconSize={14} />
         Add another card
       </button>
