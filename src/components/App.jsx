181e4b56c0641e7073cd9bf3997272bb695e53dd
VasylPopaz <pafos1997@gmail.com>
2024-04-21 15:55:32 +0200

    added private and public routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-// import { PublicRoute, PrivateRoute } from '../routes';
+import { PublicRoute, PrivateRoute } from '../routes';
 import { TestPage } from '../pages/TestPage/TestPage';
 import { lazy, useEffect } from 'react';
 import SharedLayout from './SharedLayout/SharedLayout';
 import DashboardPage from '../pages/DashboardPage/DashboardPage.jsx';
 import VerifyPage from '../pages/VerifyPage/VerifyPage';
 import Sidebar from './Sidebar/Sidebar';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { refreshThunk } from '../redux/auth/operations.js';
+import { selectIsLoading } from '../redux/auth/slice.js';
 
 const WelcomePage = lazy(() => import('pages/WelcomePage/WelcomePage'));
 const AuthPage = lazy(() => import('pages/AuthPage/AuthPage'));
@@ -16,33 +17,67 @@ const HomePage = lazy(() => import('pages/HomePage/HomePage'));
 
 const App = () => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
     dispatch(refreshThunk());
   }, [dispatch]);
 
-  // const isLoggedIn = useSelector(selectIsLoggedIn);
-  // const isLoggedIn = true;
-
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route path="/welcome" element={<WelcomePage />} />
-        <Route path="/auth/:id" element={<AuthPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/home/:boardName" element={<DashboardPage />} />
-        <Route path="/sidebar" element={<Sidebar />} />
-        <Route path="/home/:boardName" element={<h2>ScreensPage</h2>} />
-        <Route path="/test" element={<TestPage />} />
-        <Route path="/auth/verifyLogin/:messageCode" element={<VerifyPage />} />
-        <Route path="*" element={<Navigate to="welcome" replace />} />
-        <Route path="/" element={<Navigate to="welcome" replace />} />
-        {/* <Route
-          path="/"
-          element={<Navigate to={isLoggedIn ? 'home' : 'welcome'} replace />}
-        /> */}
-      </Route>
-    </Routes>
+    !isLoading && (
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route
+            path="/welcome"
+            element={
+              <PublicRoute>
+                <WelcomePage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/auth/:id"
+            element={
+              <PublicRoute>
+                <AuthPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute>
+                <HomePage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/home/:boardName"
+            element={
+              <PrivateRoute>
+                <DashboardPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/sidebar" element={<Sidebar />} />
+          <Route
+            path="/home/:boardName"
+            element={
+              <PrivateRoute>
+                <h2>ScreensPage</h2>
+              </PrivateRoute>
+            }
+          />
+          <Route path="/test" element={<TestPage />} />
+          <Route
+            path="/auth/verifyLogin/:messageCode"
+            element={<VerifyPage />}
+          />
+          <Route path="*" element={<Navigate to="welcome" replace />} />
+          <Route path="/" element={<Navigate to="welcome" replace />} />
+        </Route>
+      </Routes>
+    )
   );
 };
 export default App;
--- src/schemas/EditBoardSchema.js
+++ src/schemas/EditBoardSchema.js
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 
 export const Schema = Yup.object().shape({
-  title: Yup.string()
+  name: Yup.string()
     .min(3, 'Your title should be 3 or more characters.')
     .max(64, 'Your title should not be more than 64 characters.')
     .required('This field is required.'),
