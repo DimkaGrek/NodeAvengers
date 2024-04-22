9babc5cf7e091b958c315d4be07472f01b61535c
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-04-22 20:43:52 +0300

    added modal add bodard

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,12 +1,37 @@
 import s from './DashboardPage.module.css';
 
 import DashboardHeader from '../../components/DashboardHeader/DashboardHeader.jsx';
-import { useState } from 'react';
 import CardsColumn from '../../components/CardsColumn/CardsColumn.jsx';
 import AddColumnButton from '../../components/AddColumnButton/AddColumnButton.jsx';
 
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getBoards } from '../../redux/boards/boardsOperations';
+import { getBoard } from '../../redux/boards/boardsOperations';
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 const DashboardPage = () => {
   const [cardsColumns, setCardsColumns] = useState([]);
+  const { boardName } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(getBoards())
+      .unwrap()
+      .then(res => {
+        console.log(res);
+        const selectedBoard = res.find(board => board.name === boardName);
+
+        if (!selectedBoard) {
+          navigate('/404');
+          return;
+        }
+
+        dispatch(getBoard(selectedBoard._id));
+      });
+  }, [boardName, dispatch, navigate]);
 
   const handleAddColumn = () => {
     setCardsColumns([
