2df915406fb1e103b3c2a5b279935ae95bcd7266
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2024-04-25 21:44:00 +0300

    Update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -97,11 +97,9 @@ export const resetPasswordThunk = createAsyncThunk(
       const result = await api.post('/auth/resendPassword', email);
       return result.data.message;
     } catch (error) {
-      console.log('error.response===> ', error.response);
-      console.log('operations ', error);
-      console.log('kukukuukuukuku');
       let err;
-      if (error.response) err = error.response.data.message;
+      // eslint-disable-next-line no-prototype-builtins
+      if (error.hasOwnProperty('response')) err = error.response.data.message;
       else err = error.message;
       return thunkAPI.rejectWithValue(err);
     }
