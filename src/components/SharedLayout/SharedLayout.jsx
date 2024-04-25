5efc6f9201145602fc50b2993cb8770f56d07711
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-04-25 23:03:57 +0300

    changed bg for board

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -5,11 +5,18 @@ import Sidebar from '../Sidebar/Sidebar';
 
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from '../../redux/auth/slice';
+import { selectCurrentBoard } from '../../redux/boards/boardsSlice';
+import { getImages } from '../../helpers';
+import { getBgUrls } from '../../helpers/getBgUrl';
 
 import s from './SharedLayout.module.css';
 
 const SharedLayout = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  const board = useSelector(selectCurrentBoard)?.backgroundImage;
+  const bgImage = getImages().bg[board];
+  const backgroundImage = getBgUrls(bgImage);
 
   if (!isLoggedIn) {
     return (
@@ -27,7 +34,19 @@ const SharedLayout = () => {
       <aside>
         <Sidebar />
       </aside>
-      <main>
+      <main
+        className={s.pageWrapper}
+        style={
+          board && {
+            '--background-image-desk-1x': `url(${backgroundImage[0]})`,
+            '--background-image-desk-2x': `url(${backgroundImage[1]})`,
+            '--background-image-tab-1x': `url(${backgroundImage[2]})`,
+            '--background-image-tab-2x': `url(${backgroundImage[3]})`,
+            '--background-image-mob-1x': `url(${backgroundImage[4]})`,
+            '--background-image-mob-2x': `url(${backgroundImage[5]})`,
+          }
+        }
+      >
         <Suspense fallback={null}>
           <Outlet />
         </Suspense>
