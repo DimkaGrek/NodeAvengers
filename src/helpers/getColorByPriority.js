202e775170a8d398ac0e3c71c0da575007e9f428
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-04-29 10:51:32 +0300

    fixed priority flag color

--- src/helpers/getColorByPriority.js
+++ src/helpers/getColorByPriority.js
@@ -1,7 +1,7 @@
 export const getColorByPriority = priority => {
   switch (priority) {
     case 'without':
-      return 'rgba(22, 22, 22, 0.3)';
+      return 'rgba(111, 105, 105, 0.3)';
     case 'low':
       return '#8fa1d0';
     case 'medium':
@@ -9,6 +9,6 @@ export const getColorByPriority = priority => {
     case 'high':
       return '#bedbb0';
     default:
-      return 'rgba(22, 22, 22, 0.3)';
+      return 'rgba(111, 105, 105, 0.3)';
   }
 };
